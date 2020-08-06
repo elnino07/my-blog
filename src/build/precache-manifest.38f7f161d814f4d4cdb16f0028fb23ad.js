aeec42324da1ad8dfc6d2775119bfcae2c58043f
Marc Trawetzky <marc.trawetzky@fusonic.net>
2020-08-06 13:21:55 +0200

    First commit

---
+++ src/build/precache-manifest.38f7f161d814f4d4cdb16f0028fb23ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7925253c73cffffa84a7fb832845a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04fdccbb0cb291d9609c",
+    "url": "/static/css/main.d301e858.chunk.css"
+  },
+  {
+    "revision": "50d8325b492adc7f0d2c",
+    "url": "/static/js/2.977e4e9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.977e4e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04fdccbb0cb291d9609c",
+    "url": "/static/js/main.cc553ed1.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
